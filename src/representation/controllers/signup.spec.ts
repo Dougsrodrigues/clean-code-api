49887993b0465be5626d752b0bd9c3b48ce2d4fd
Douglas Santiago <dougsrodrigues@outlook.com>
2020-07-18 14:44:08 -0300

    chore: add eslint rule

---
+++ src/representation/controllers/signup.spec.ts
@@ -0,0 +1,16 @@
+import SignuUpController from './signup';
+
+describe('SignUp Controller', () => {
+  test('Should  return 400 if no name is provider', () => {
+    const sut = new SignuUpController();
+    const httpRequest = {
+      email: 'any_email@email.com',
+      password: 'any_password',
+      passwordConfirmation: 'any_password',
+    };
+
+    const httpResponse = sut.handle(httpRequest);
+
+    expect(httpResponse.statusCode).toBe(400);
+  });
+});
---
+++ src/representation/controllers/signup.ts
@@ -0,0 +1,9 @@
+class SignuUpController {
+  handle(httpRequest: any): any {
+    return {
+      statusCode: 400,
+    };
+  }
+}
+
+export default SignuUpController;
