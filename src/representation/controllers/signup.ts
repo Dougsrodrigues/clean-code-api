11dea6bfcec098c07ae9e28b89224b9b37aa47a3
Douglas Santiago <dougsrodrigues@outlook.com>
2020-07-20 20:52:58 -0300

    feat: ensure signupController returns 500 if EmailValidator throws

--- src/representation/controllers/signup.ts
+++ src/representation/controllers/signup.ts
@@ -4,6 +4,7 @@ import badRequest from '../helpers/http-helper';
 import { Controller } from '../protocols/controllers';
 import { EmailValidator } from '../protocols/email-validator';
 import InvalidParamError from '../errors/invalid-param-error';
+import ServerError from '../errors/server-error';
 
 class SignUpController implements Controller {
   private readonly emailValidator: EmailValidator;
@@ -13,26 +14,33 @@ class SignUpController implements Controller {
   }
 
   handle(httpRequest: HttpRequest): HttpResponse {
-    const requiredFields = [
-      'name',
-      'email',
-      'password',
-      'passwordConfirmation',
-    ];
+    try {
+      const requiredFields = [
+        'name',
+        'email',
+        'password',
+        'passwordConfirmation',
+      ];
 
-    for (const field of requiredFields) {
-      if (!httpRequest.body[field]) {
-        return badRequest(new MissingParamError(field));
+      for (const field of requiredFields) {
+        if (!httpRequest.body[field]) {
+          return badRequest(new MissingParamError(field));
+        }
       }
+
+      const isValid = this.emailValidator.isValid(httpRequest.body.email);
+
+      if (!isValid) {
+        return badRequest(new InvalidParamError('email'));
+      }
+
+      return badRequest(new Error('SignUp Gerenic Error'));
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: new ServerError(),
+      };
     }
-
-    const isValid = this.emailValidator.isValid(httpRequest.body.email);
-
-    if (!isValid) {
-      return badRequest(new InvalidParamError('email'));
-    }
-
-    return badRequest(new Error('SignUp Gerenic Error'));
   }
 }
 
