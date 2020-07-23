a79fb826697c72fbcb2e14d6f3e6c31d463a3687
Douglas Santiago <dougsrodrigues@outlook.com>
2020-07-22 22:25:49 -0300

    feat: ensure SignUpController returns 200 if valid data is provided

--- src/representation/controllers/signUp/signup.ts
+++ src/representation/controllers/signUp/signup.ts
@@ -47,13 +47,13 @@ class SignUpController implements Controller {
         return badRequest(new InvalidParamError('email'));
       }
 
-      this.addAccount.add({
+      const account = this.addAccount.add({
         name,
         email,
         password,
       });
 
-      return badRequest(new Error('SignUp Gerenic Error'));
+      return { statusCode: 200, body: account };
     } catch (error) {
       return serverError();
     }
